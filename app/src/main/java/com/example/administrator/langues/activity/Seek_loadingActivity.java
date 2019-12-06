ce1583ed1cf836dfd3178bac48860ff9d4b52694
Mr.yang <2524522304@qq.com>
2019-12-06 11:06:53 +0800

    完善匹配界面（2）

--- app/src/main/java/com/example/administrator/langues/activity/Seek_loadingActivity.java
+++ app/src/main/java/com/example/administrator/langues/activity/Seek_loadingActivity.java
@@ -1,28 +1,41 @@
 package com.example.administrator.langues.activity;
 
-import android.os.Bundle;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.administrator.langues.R;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class Seek_loadingActivity extends AppCompatActivity {
     private static int START_ANIMATION=0;
+    private TextView seek_loading_text;//匹配中....
     private ImageButton cancel_btn;
+    private CircleImageView image_btn;
     private ImageView icon1,icon2,icon5,icon3,icon7;
     private Animation animation1 = null;
     private Animation animation2 = null;
     private Animation animation3 = null;
+    private Animation animation4 = null;
+    private Animation animation5 = null;
+    private ObjectAnimator objectAnimator1=null;
 
     private Timer timer;
     private Handler mHandler=new Handler(){
@@ -36,14 +49,14 @@ public class Seek_loadingActivity extends AppCompatActivity {
                 icon7.clearAnimation();
                 /****************************************/
                 icon1.startAnimation(animation2);
-                icon2.startAnimation(animation2);
+                icon2.startAnimation(animation5);
                 icon3.startAnimation(animation1);
-                icon5.startAnimation(animation2);
+                icon5.startAnimation(animation4);
                 icon7.startAnimation(animation3);
 
             }
         }
-        };
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +66,14 @@ public class Seek_loadingActivity extends AppCompatActivity {
         animation();
         timer=new Timer();
         startTime();
+        //匹配中动画
+        objectAnimator1.setDuration(2000l);
+        objectAnimator1.setRepeatCount(-1);
+        objectAnimator1.start();
+
+
+
+        //取消匹配
         cancel_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,11 +82,24 @@ public class Seek_loadingActivity extends AppCompatActivity {
 
             }
         });
+
+        image_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getBaseContext(),Seek_successActivity.class);
+                closeTime();
+                startActivity(i);
+            }
+        });
     }
+
     private void animation() {
         animation1 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.anim_rotate_seek);
-        animation2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.anim_alpha);
+        animation2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.anim_translate_seek);
         animation3 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.anim_rerotate_seek);
+        animation4 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.anim_retranslate_seek);
+        animation5 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.anim_translate1_seek);
+        objectAnimator1=ObjectAnimator.ofFloat(seek_loading_text,"translationX",20f,40f,20f);
     }
 
     private void init() {
@@ -75,14 +109,16 @@ public class Seek_loadingActivity extends AppCompatActivity {
         icon2=findViewById(R.id.icon2);
         icon5=findViewById(R.id.icon5);
         icon7=findViewById(R.id.icon7);
+        seek_loading_text=findViewById(R.id.seek_loading_text);
         cancel_btn=findViewById(R.id.cancel_btn);
+        image_btn=findViewById(R.id.seek_loading_user);
 
     }
     public  void startTime(){
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                Message m=new Message();
+                Message m=Message.obtain();
                 m.what=START_ANIMATION;
                 mHandler.sendMessage(m);
             }
---
+++ app/src/main/java/com/example/administrator/langues/activity/Seek_successActivity.java
@@ -0,0 +1,134 @@
+package com.example.administrator.langues.activity;
+
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.example.administrator.langues.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Seek_successActivity extends AppCompatActivity {
+    private final static int START_ANIMATION=0;
+    private final static int STOP_ANIMATION=1;
+    private ImageView seeksuccess_icon;
+    private RelativeLayout red_user,blue_user;
+    private Timer timer;
+    private int width,height;
+    private ObjectAnimator objectAnimator1=null;
+    private ObjectAnimator objectAnimator2=null;
+    private ObjectAnimator objectAnimator3=null;
+    private ObjectAnimator objectAnimator4=null;
+    private ObjectAnimator objectAnimator5=null;
+    private ObjectAnimator objectAnimator6=null;
+    private ObjectAnimator objectAnimator7=null;
+    private AnimatorSet animSet;
+    private AnimatorSet animSet1;
+    private AnimatorSet animSet2;
+    private Handler mHandler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case START_ANIMATION:
+
+                    break;
+                case STOP_ANIMATION:
+
+                    break;
+            }
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seek_success);
+        init();
+        animation();
+        getdisplay();
+        start();
+        timer=new Timer();
+        startTime();
+
+
+
+
+
+    }
+
+    private void getdisplay() {
+        // 通过WindowManager获取屏幕的宽高
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        width=dm.widthPixels;
+        height=dm.heightPixels;
+    }
+
+    private void start() {
+        animSet= new AnimatorSet();
+        animSet.play(objectAnimator1).with(objectAnimator2).with(objectAnimator3);
+        animSet.setDuration(3000l);
+        animSet.start();
+        animSet1=new AnimatorSet();
+        animSet1.play(objectAnimator4).with(objectAnimator5);
+        animSet1.setDuration(3000l);
+        animSet1.start();
+        animSet2=new AnimatorSet();
+        animSet2.play(objectAnimator6).with(objectAnimator7);
+        animSet2.setDuration(3000l);
+        animSet2.start();
+        (new Timer()).schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        },0,3000);
+
+
+    }
+
+    private void animation() {
+        //飞机动画
+        objectAnimator1=ObjectAnimator.ofFloat(seeksuccess_icon,"scaleX",0f,20f);
+        objectAnimator2=ObjectAnimator.ofFloat(seeksuccess_icon,"scaleY",0f,20f);
+        objectAnimator3=ObjectAnimator.ofFloat(seeksuccess_icon,"alpha",0f);
+        //红色用户
+        objectAnimator4=ObjectAnimator.ofFloat(red_user,"translationX",600f,0f);
+        objectAnimator5=ObjectAnimator.ofFloat(red_user,"translationY",-900f,272f);
+        //蓝色用户
+        objectAnimator6=ObjectAnimator.ofFloat(blue_user,"translationX",-600f,0f);
+        objectAnimator7=ObjectAnimator.ofFloat(blue_user,"translationY",900f,-272f);
+
+
+    }
+
+
+    private void init() {
+        seeksuccess_icon=findViewById(R.id.seeksuccess_icon);
+        red_user=findViewById(R.id.red_user);
+        blue_user=findViewById(R.id.blue_user);
+    }
+    public  void startTime(){
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Message m=Message.obtain();
+                m.what=START_ANIMATION;
+                mHandler.sendMessage(m);
+            }
+        },0,4000);
+    }
+    public void closeTime(){
+        timer.cancel();
+
+    }
+}
