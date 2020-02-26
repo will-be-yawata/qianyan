7a4188809b3dda5d13de41be50b65f9654148c0c
Mr.yang <2524522304@qq.com>
2020-02-26 22:02:37 +0800

    聊天界面完善2(聊天气泡完善)

---
+++ app/src/main/java/com/example/administrator/langues/activity/Chat/Friends_settingActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.langues.activity.Chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.langues.R;
+
+public class Friends_settingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_setting);
+    }
+}
