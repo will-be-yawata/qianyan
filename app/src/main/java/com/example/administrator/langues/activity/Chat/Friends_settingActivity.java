566589729c3615282af062bb629c8457469aad87
Mr.yang <2524522304@qq.com>
2020-03-02 16:35:53 +0800

    聊天界面完善5(好友名片)

--- app/src/main/java/com/example/administrator/langues/activity/Chat/Friends_settingActivity.java
+++ app/src/main/java/com/example/administrator/langues/activity/Chat/Friends_settingActivity.java
@@ -1,18 +1,23 @@
 package com.example.administrator.langues.activity.Chat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.administrator.langues.R;
+import com.example.administrator.langues.activity.MyPage.User_data.Personal_cardActivity;
 
 public class Friends_settingActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageButton top_chat_close,top_chat_open;
     private ImageButton not_disturb_close,not_disturb_open;
+    private ImageButton friend_setting_return;
     private Button delete_friend;
+    private RelativeLayout personal_card;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,8 @@ public class Friends_settingActivity extends AppCompatActivity implements View.O
         not_disturb_close.setOnClickListener(this);
         not_disturb_open.setOnClickListener(this);
         delete_friend.setOnClickListener(this);
+        personal_card.setOnClickListener(this);
+        friend_setting_return.setOnClickListener(this);
     }
 
     private void initView() {
@@ -37,11 +44,21 @@ public class Friends_settingActivity extends AppCompatActivity implements View.O
         not_disturb_close=findViewById(R.id.not_disturb_close);
         not_disturb_open=findViewById(R.id.not_disturb_open);
         delete_friend=findViewById(R.id.delete_friend);
+        personal_card=findViewById(R.id.personal_card);
+        friend_setting_return=findViewById(R.id.friend_setting_return);
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.friend_setting_return:
+                finish();
+                break;
+            case R.id.personal_card:
+                Intent intent=new Intent(getBaseContext(),Personal_cardActivity.class);
+                startActivity(intent);
+                break;
             case R.id.top_chat_close:
                 top_chat_close.setVisibility(View.GONE);
                 top_chat_open.setVisibility(View.VISIBLE);
@@ -72,6 +89,7 @@ public class Friends_settingActivity extends AppCompatActivity implements View.O
                     }
                 }.show();
                 break;
+
         }
         
     }
